7f1511b496f4cde63839b17425c86e32e0050cca
Iulia Iasoveeva <yulia.yasoveeva@gmail.com>
2022-02-19 19:36:55 +0100

    Add ReverseArgs

---
+++ src/main/java/com/epam/training/student_iulia_iasoveeva/fundamentals/ReverseArgs.java
@@ -0,0 +1,17 @@
+package com.epam.training.student_iulia_iasoveeva.fundamentals;
+
+import java.util.Scanner;
+
+public class ReverseArgs {
+    public static void main(String[] args) {
+        String arg, arg1, arg2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first argument: ");
+        arg = scanner.nextLine();
+        System.out.println("Enter second argument: ");
+        arg1 = scanner.nextLine();
+        System.out.println("Enter third argument: ");
+        arg2 = scanner.nextLine();
+        System.out.println("Result: " + arg2 + " " + arg1 + " " + arg);
+    }
+}
