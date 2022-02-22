ac00b85d0e1c365069be0d937f5ae1bfe0aba42f
Iulia Iasoveeva <yulia.yasoveeva@gmail.com>
2022-02-22 12:08:12 +0100

    Add NumberByLength

--- src/main/java/com/epam/training/student_iulia_iasoveeva/fundamentals/optional_task1/NumberByLength.java
+++ src/main/java/com/epam/training/student_iulia_iasoveeva/fundamentals/optional_task1/NumberByLength.java
@@ -1,19 +1,39 @@
-package com.epam.training.student_iulia_iasoveeva.fundamentals.opsional_task1;
+package com.epam.training.student_iulia_iasoveeva.fundamentals.optional_task1;
 
 import java.util.Scanner;
 
-public class numberByLenght {
+public class NumberByLength {
     public static void main(String[] args) {
         int[] array;
         int n;
+        int[] size;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter quantity of numbers: ");
         n = scanner.nextInt();
+        System.out.println("Enter numbers: ");
         array = new int[n];
+        size = new int[n];
         for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
+            int a = scanner.nextInt();
+            array[i] = a;
+            size[i] = String.valueOf(a).length();
         }
-          }
-    int length = String.valueOf(array).length();
-          
+        System.out.println("Length of numbers: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(size[i]);
+        }
+        int elementMin = 0;
+        int elementMax = 0;
+        for (int i = 1; i < size.length; i++) {
+            if (size[elementMin] > size[i]) {
+                elementMin = i;
+            }
+            if (size[elementMax] < size[i]) {
+                elementMax = i;
+            }
+        }
+
+        System.out.println("The shortest number: " + array[elementMin]);
+        System.out.print("The longest number: " + array[elementMax]);
+    }
 }
