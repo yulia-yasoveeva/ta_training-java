355a167fa5afc25523cc9c789a1355e2d4df7c4d
Iulia Iasoveeva <yulia.yasoveeva@gmail.com>
2022-02-22 14:17:33 +0100

    Add SortdeLenght

---
+++ src/main/java/com/epam/training/student_iulia_iasoveeva/fundamentals/optional_task1/SortedLength.java
@@ -0,0 +1,53 @@
+package com.epam.training.student_iulia_iasoveeva.fundamentals.optional_task1;
+
+import java.util.Scanner;
+
+public class SortedLength {
+    public static void main(String[] args) {
+        int[] array;
+        int n;
+        int[] size;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter quantity of numbers: ");
+        n = scanner.nextInt();
+        System.out.println("Enter numbers: ");
+        array = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            array[i] = a;
+            size[i] = String.valueOf(a).length();
+        }
+        System.out.println("Length of numbers: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(size[i]);
+        }
+        int elementMin = 0;
+        int elementMax = 0;
+        for (int i = 1; i < size.length; i++) {
+            if (size[elementMin] > size[i]) {
+                elementMin = i;
+            }
+            if (size[elementMax] < size[i]) {
+                elementMax = i;
+            }
+        }
+        System.out.println("Sorted array in ascending order: ");
+        for (int i = size[elementMin]; i <= size[elementMax]; i++) {
+            for (int j = 0; j < size.length; j++) {
+                if (size[j] == i) {
+                    System.out.println(array[j]);
+                }
+            }
+        }
+        System.out.println("Sorted array in descending order: ");
+        for (int i = size[elementMax]; i >= size[elementMin]; i--) {
+            for (int j = 0; j < size.length; j++) {
+                if (size[j] == i) {
+                    System.out.println(array[j]);
+                }
+            }
+        }
+    }
+}
+
